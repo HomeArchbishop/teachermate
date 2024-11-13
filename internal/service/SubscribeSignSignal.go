1aabf38abfa473b663fdbc06d8ef133e29b82275
HomeArchbishop <1984096954@qq.com>
2024-11-13 15:02:54 +0800

    fix(server): reject the connection when same subscription exists

--- internal/service/SubscribeSignSignal.go
+++ internal/service/SubscribeSignSignal.go
@@ -11,6 +11,14 @@ func SubscribeSignSignal(lessonId, studentId, connId string) *WebError {
 		return NewWebError(400, "empty arguments")
 	}
 
+	if has, err := model.HasSubscription(lessonId, studentId); err != nil {
+		log.Println(err)
+		return NewWebError(500, "internal error")
+	} else if has {
+		// already subscribed
+		return NewWebError(400, "already subscribed")
+	}
+
 	if err := model.AddSubscription(lessonId, studentId, connId); err != nil {
 		log.Println(err)
 		return NewWebError(500, "internal error")
