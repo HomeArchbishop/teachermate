acc6cb68d630f5b06feb81ee99dc85e6a597866e
HomeArchbishop <1984096954@qq.com>
2024-11-13 15:31:19 +0800

    feat(server): use Msg json to send data

--- internal/controller/Sign.go
+++ internal/controller/Sign.go
@@ -1,19 +1,33 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 	"github.com/homearchbishop/teachermate-auto/internal/service"
+	"github.com/homearchbishop/teachermate-auto/internal/shared"
 )
 
 func signHandler(w http.ResponseWriter, r *http.Request) {
 	lessonId := r.URL.Query().Get("lesson_id")
-	attendanceId := r.URL.Query().Get("attendance")
+	signUrl := r.URL.Query().Get("attendance")
 
 	connIdList, webErr := service.GetSubscriptionConns(lessonId)
 	if webErr != nil {
 		http.Error(w, webErr.Message, webErr.Code)
+		return
+	}
+
+	signSignalMsg, err := shared.CreateSignSignalMsg(signUrl)
+	if err != nil {
+		http.Error(w, "internal error: "+err.Error(), 500)
+		return
+	}
+
+	signSignalMsgJson, err := json.Marshal(signSignalMsg)
+	if err != nil {
+		http.Error(w, "internal error", 500)
 		return
 	}
 
@@ -23,7 +37,7 @@ func signHandler(w http.ResponseWriter, r *http.Request) {
 			// no need to check error,
 			// Because PingMessage will check the connection status
 			// and close the connection if it is not available.
-			conn.WriteMessage(websocket.TextMessage, []byte(attendanceId))
+			conn.WriteMessage(websocket.TextMessage, signSignalMsgJson)
 		}
 	}
 	connList.mux.Unlock()
